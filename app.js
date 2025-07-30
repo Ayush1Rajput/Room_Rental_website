5996b3f3fdba6cee8d5222544bf60a184c60b411
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-30 22:39:23 +0530

    Add new route for add new data from user

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.set('view-engine',"ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 // show All list/ data
 app.get('/listings',async (req,res)=>{
@@ -33,6 +33,11 @@ app.get('/listings',async (req,res)=>{
  
 })
 
+// For Creating new data 
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
 // show perticular data information 
 app.get("/listings/:id",async (req,res)=>{
   let {id} = req.params;
@@ -40,6 +45,14 @@ app.get("/listings/:id",async (req,res)=>{
   res.render("listings/show.ejs",{listing});
 
 })
+  
+// for save new data
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
 
 app.listen(5050, () => {
   console.log("Server running on 5050");
