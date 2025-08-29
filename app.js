74063d8622a9e310f7e62d233ddd7fd7fac21db3
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-08-29 14:57:19 +0530

    Restructure the reviews route

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
 
 const listings  = require('./routes/listing.js');
+const reviews = require('./routes/review.js');
 
 main()
   .then(() => {
@@ -34,46 +30,9 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
 
-
-// Validation for Review Schema
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
 app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
-// Reviews
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    console.log("Review Saved");
-    res.redirect(`/listings/${listing._id}`);
-  })
-);
-
-// Delete Review Route
-app.delete('/listings/:id/reviews/:reviewId', wrapAsync( async (req,res)=>{
-  let {id, reviewId} = req.params;
-  Listing.findByIdAndUpdate(id, {$pull : {reviews:reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-
-  res.redirect(`/listings/${id}`)
-}))
 
 // app.all("*", (req,res, next)=>{
 //   next(new ExpressError(404, "Page Not Found !"));
