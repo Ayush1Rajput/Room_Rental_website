3ae1dab319bd447a10bc953058fd9dee4a8f0ac6
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-04 17:30:57 +0530

    Add a middleware for validateListings

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
 const Listing = require("./models/listing");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -27,3 +29,14 @@ module.exports.isOwner = async (req,res,next)=>{
 
     next();
 }
+
+// Validation for listing Schema
+module.exports.validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
--- routes/listing.js
+++ routes/listing.js
@@ -2,22 +2,11 @@ const express = require('express');
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
-const { listingSchema } = require("../schema.js");
-const ExpressError = require("../utils/ExpressError.js");
-const {isLoggedIn, isOwner} = require('../middleware.js');
+const {isLoggedIn, isOwner,validateListing} = require('../middleware.js');
 
 
 
-// Validation for listing Schema
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+
 
 
 // show All list/ data
