d5ab40e50667ef28ef7fd2c8cea53e89db2c632b
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-04 17:33:54 +0530

    Add a middleware for validateReview

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const Listing = require("./models/listing");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema,reviewSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -33,6 +33,17 @@ module.exports.isOwner = async (req,res,next)=>{
 // Validation for listing Schema
 module.exports.validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+// Validation for Review Schema
+module.exports.validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
   if (error) {
     let errMsg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(400, errMsg);
