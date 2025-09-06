a0e02296ca4fd084e59d60dd313c69cb94532c52
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-06 23:28:10 +0530

    Integrate multer middleware with Cloudinary storage configuration

--- routes/listing.js
+++ routes/listing.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const {storage} = require('../cloudConfig.js');
+const upload = multer({storage});
 
 const listingController = require("../controllers/listings.js");
 
