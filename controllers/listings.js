0518a7a1773dac49bcfb4405fb7e05c7045dca79
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-09-11 11:11:52 +0530

    Add Geometry and cordinates in listings model and make changes in routes

--- controllers/listings.js
+++ controllers/listings.js
@@ -39,15 +39,17 @@ module.exports.createListing = async (req, res, next) => {
     })
     .send();
 
-  console.log(response);
-
+  // console.log(response);
   let url = req.file.path;
   let filename = req.file.filename;
 
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
-  await newListing.save();
+
+  newListing.geometry = response.body.features[0].geometry;
+
+  let savedListing = await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
+const { required } = require("joi");
 
 const listingSchema = new mongoose.Schema({
   title: {
@@ -25,6 +26,17 @@ const listingSchema = new mongoose.Schema({
   owner: {
     type: Schema.Types.ObjectId,
     ref: "User",
+  },
+  geometry:{
+    type:{
+      type:String,
+      enum: ["Point"],
+      required:true,
+    },
+    coordinates:{
+      type:[Number],
+      required:true,
+    },
   },
 });
 
